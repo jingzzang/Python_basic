ad0c465942c07a336fca589d6c77083e0dc4bcd8
Hi <Hi>
2020-06-08 22:46:45 +0900

    function

---
+++ function.py
@@ -0,0 +1,13 @@
+def solution(n1,n2):
+        sum=n1+n2
+        mul=n1*n2
+        return sum+mul
+
+def length(s1,s2):
+        a=len(s1)
+        b=len(s2)
+        return a+b
+
+str1=input("?")
+str2=input("?")
+print(length(str1,str2))
