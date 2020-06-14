0a5ed328a975da00d1b34df6ff6b0f38bea2de49
Hi <Hi>
2020-06-14 19:37:53 +0900

    Test 1

--- test1_2.py
+++ test1_2.py
@@ -1,5 +1,31 @@
+#4
+def func(list):
+    tmp=[]
+    for i in list:
+        if i!=0:
+            tmp.append(i)
+    return tmp
+
+def sol4(list):
+    cnt=[0 for _ in range(1001)]
+    for i in list:
+        cnt[i]+=1
+    li=func(cnt)
+    return max(li)//min(li)
+arr4=[1,2,3,3,1,3,3,2,3,2]
+print(sol4(arr4))
+
+#5
+arr5=[1,4,2,3]
+def sol5(list):
+    ans=[]
+    for i in range(len(list)):
+        ans.append(list[-(i+1)])
+    return ans
+print(sol5(arr5))
+
 #6
-def solution(n):
+def sol6(n):
     cnt=0
     for i in range(1,n+1):
         c=i
@@ -9,11 +35,22 @@ def solution(n):
             c//=10
     return cnt
 n=int(input(":"))
-print(solution(n))
+print(sol6(n))
+
+#7
+score=[650, 722, 914, 558, 714, 803, 650, 679, 669, 800]
+def sol7(scores):
+    cnt=0
+    for i in scores:
+        if i>=650 and i<800:
+            cnt+=1
+    return cnt
+print(sol7(score))
 
 #8
-str="never odd or even"
-def sol(s):
+#str="never odd or even"
+str="palindrome"
+def sol8(s):
     tmp=[]
     for i in s:
         if i!='.' and i!=' ':
@@ -23,4 +60,15 @@ def sol(s):
             return "X"
             break
     return "O"
-print(sol(str))
+print(sol8(str))
+
+#9
+#str="senteeeencccccceeee"
+str="senteeeencccccceieee"
+def sol9(s):
+    ans=s[0]
+    for i in range(1,len(s)):
+        if s[i-1]!=s[i]:
+            ans+=s[i]
+    return ans
+print(sol9(str))
